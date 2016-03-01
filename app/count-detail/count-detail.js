b4e86ba125e02fb2923c1dde11aa68071eac5e81
gfed53 <gfederico7153@gmail.com>
2016-03-01 16:18:06 -0500

    Bug fix: neighboring countries lookup produces capital population

--- app/count-detail/count-detail.js
+++ app/count-detail/count-detail.js
@@ -1,50 +1,50 @@
 angular.module('ccAppViews')
 .config(['$routeProvider', function($routeProvider){
 	$routeProvider
-	.when("/countries/:country/capital", {
+	.when("/countries/:country", {
 		templateUrl : "./count-detail/count-detail.html",
 		controller : "CountDetailCtrl",
-		// resolve: {
-		// 	country : ['$route', function($route){
+		// resolve : {
+		// 	country2 : ['$route', function($route){
 		// 		return $route.current.params.country;
 		// }]}
-	})
-	.when("/countries/:neighbor/capital", {
-		templateUrl : "./count-detail/count-detail.html",
-		controller : "CountDetailCtrl",
-		// resolve: {
-		// 	country : ['$route', function($route){
-		// 		return $route.current.params.country;
-		// }]}
-	})
+	});
 }])
 
 .controller('CountDetailCtrl', ['$scope', '$route', 'ccCountryDetail', 'ccCapDetail', 'ccNeighborDetail', function($scope, $route, ccCountryDetail, ccCapDetail, ccNeighborDetail){
-	if($route.current.params.neighbor){
-		$scope.country = $route.current.params.neighbor;
-	} else {
-		$scope.country = $route.current.params.country;
-	}
-	
+	$scope.country = $route.current.params.country;
 	$scope.countryObj = JSON.parse($scope.country);
+	//We're using the params of the neighbor, that's why we're not getting capital back.
+	console.log($scope.countryObj);
 
 	ccCountryDetail($scope.countryObj.countryCode)
 	.then(function(country){
-		$scope.country = country.geonames[0];
-	});
+			$scope.country = country.geonames[0];
+			console.log($scope.country);
+			ccCapDetail($scope.country.capital, $scope.country.countryCode)
+			.then(function(capital){
+				console.log(capital);
+				$scope.capital = capital.geonames[0];
+				console.log($scope.capital);
+			});
+		}
+	);
 
-	ccCapDetail($scope.countryObj.capital, $scope.countryObj.countryCode)
-	.then(function(capital){
-		console.log(capital);
-		$scope.capital = capital.geonames[0];
-		console.log($scope.capital);
-	});
+	//This is running simultaniously with ccCountryDetail, that's why $scope.country won't be changed before this runs.
+	// ccCapDetail($scope.countryObj.capital, $scope.countryObj.countryCode)
+	// .then(function(capital){
+	// 	console.log(capital);
+	// 	if(capital.geonames){
+	// 		$scope.capital = capital.geonames[0];
+	// 	}
+	// 	console.log($scope.capital);
+	// });
 
 	ccNeighborDetail($scope.countryObj.countryCode)
 	.then(function(neighbors){
 		console.log(neighbors);
 		$scope.neighbors = neighbors.geonames;
-	})
+	});
 
 	console.log("This is a detailed view of the selected country");
 }]);
