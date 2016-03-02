456d1b23b0e3adfbd1f30501f7ceb6a1e961392d
gfed53 <gfederico7153@gmail.com>
2016-03-02 17:53:28 -0500

    Added loading/transition animations

--- app/cc-app.js
+++ app/cc-app.js
@@ -3,5 +3,17 @@ angular.module('ccApp', ['ccAppViews', 'ngRoute', 'ngAnimate'])
     $locationProvider.hashPrefix('!');
     $routeProvider.otherwise({
       redirectTo : '/'
-    });
-  }]);
+    })
+}])
+
+.run(function($rootScope, $location, $timeout) {
+	    $rootScope.$on('$routeChangeStart', function() {
+	        $rootScope.isLoading = true;
+	    });
+	    $rootScope.$on('$routeChangeSuccess', function() {
+	      $timeout(function() {
+	        $rootScope.isLoading = false;
+	      }, 1000);
+	    });
+	});
+  
